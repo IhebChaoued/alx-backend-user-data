f80d3466bb5b0c90ab77a84f88c9393699b77ab7
Iheb Chaoued <ihebchaoued@gmail.com>
2024-06-04 17:55:03 +0000

    User Object

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -52,22 +52,22 @@ class BasicAuth(Auth):
         return user_email, user_password
 
     def user_object_from_credentials(
-            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+            self, user_email: str, user_pwd: str
+            ) -> TypeVar('User'):
         """
         Returns the User instance based on his email and password.
         """
-        if user_email is None or user_pwd is None:
+        if user_email is None or not isinstance(user_email, str):
             return None
-        if not isinstance(
-                user_email, str
-                ) or not isinstance(user_pwd, str):
+        if user_pwd is None or not isinstance(user_pwd, str):
             return None
 
-        users = User.search({'email': user_email})
-        if not users:
+        user_list = User.search({'email': user_email})
+        if not user_list:
             return None
 
-        for user in users:
-            if user.is_valid_password(user_pwd):
-                return user
+        user = user_list[0]
+        if not user.is_valid_password(user_pwd):
             return None
+
+        return user

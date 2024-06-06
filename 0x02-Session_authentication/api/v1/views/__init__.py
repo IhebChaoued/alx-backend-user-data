40512dd7fd1493b9a1e6c2a8521953e16cab22d5
Iheb Chaoued <ihebchaoued@gmail.com>
2024-06-06 17:42:06 +0000

    up

--- 0x02-Session_authentication/api/v1/views/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-from flask import Blueprint
-
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
-from api.v1.views.session_auth import session_auth
-app_views.register_blueprint(session_auth)

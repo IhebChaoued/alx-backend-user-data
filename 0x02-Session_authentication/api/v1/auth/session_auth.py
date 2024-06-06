ccead082337b762ef6c4d7595ad4f2f854295552
Iheb Chaoued <ihebchaoued@gmail.com>
2024-06-06 18:13:57 +0000

    logout

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,44 +1,42 @@
 #!/usr/bin/env python3
+""" SessionAuth module
 """
-SessionAuth module
-"""
-import uuid
 from api.v1.auth.auth import Auth
-from models.user import User
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
-    """
-    SessionAuth class that inherits from Auth
-    """
     user_id_by_session_id = {}
 
-    def create_session(self, user_id: str = None) -> str:
+    def create_session(self, user_id=None):
         """
-        Creates a Session ID for a given user_id
+        Create a session ID for a user_id
         """
-        if user_id is None or not isinstance(user_id, str):
+        if user_id is None or type(user_id) is not str:
             return None
-        session_id = str(uuid.uuid4())
+        session_id = str(uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
 
-    def user_id_for_session_id(self, session_id: str = None) -> str:
+    def user_id_for_session_id(self, session_id=None):
         """
         Returns a User ID based on a Session ID
         """
-        if session_id is None or not isinstance(session_id, str):
+        if session_id is None or type(session_id) is not str:
             return None
         return self.user_id_by_session_id.get(session_id)
 
-    def current_user(self, request=None):
+    def destroy_session(self, request=None):
         """
-        Returns a User instance based on a cookie value
+        Destroys the user session / logout
         """
+        if request is None:
+            return False
         session_id = self.session_cookie(request)
         if session_id is None:
-            return None
+            return False
         user_id = self.user_id_for_session_id(session_id)
         if user_id is None:
-            return None
-        return User.get(user_id)
+            return False
+        del self.user_id_by_session_id[session_id]
+        return True

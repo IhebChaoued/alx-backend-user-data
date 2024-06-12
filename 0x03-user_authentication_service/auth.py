1704cda9e731a27f32c0ab9b117bf9f1713044ee
Iheb Chaoued <ihebchaoued@gmail.com>
2024-06-12 14:51:37 +0000

    crendetials

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -27,3 +27,12 @@ class Auth:
             hashed_password = _hash_password(password)
             new_user = self._db.add_user(email, hashed_password)
             return new_user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Validate login credentials."""
+        try:
+            user = self._db.find_user_by(email=email)
+            stored_password = user.hashed_password
+            return bcrypt.checkpw(password.encode('utf-8'), stored_password)
+        except NoResultFound:
+            return False

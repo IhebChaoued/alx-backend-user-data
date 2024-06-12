2ad19791396556fb2994c1f8baa3339d165c629e
Iheb Chaoued <ihebchaoued@gmail.com>
2024-06-12 14:48:49 +0000

    user profile

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """
 Basic Flask app
 """
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 
 app = Flask(__name__)
@@ -17,15 +17,51 @@ def welcome():
 
 @app.route("/users", methods=["POST"])
 def register_user():
-    """POST route to register a user."""
-    email = request.form.get('email')
-    password = request.form.get('password')
+    """Endpoint to register a user."""
+    email = request.form.get("email")
+    password = request.form.get("password")
 
     try:
         user = AUTH.register_user(email, password)
-        return jsonify({"email": user.email, "message": "user created"})
-    except ValueError:
-        return jsonify({"message": "email already registered"}), 400
+        return jsonify({"email": user.email, "message": "user created"}), 200
+    except ValueError as e:
+        return jsonify({"message": str(e)}), 400
+
+
+@app.route("/sessions", methods=["POST"])
+def login():
+    """Endpoint to log in a user."""
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
+        if session_id:
+            response = jsonify({"email": email, "message": "logged in"})
+            response.set_cookie("session_id", session_id)
+            return response, 200
+    abort(401)
+
+
+@app.route("/profile", methods=["GET"])
+def profile():
+    """Endpoint to retrieve user profile."""
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        return jsonify({"email": user.email}), 200
+    abort(403)
+
+
+@app.route("/sessions", methods=["DELETE"])
+def logout():
+    """Endpoint to log out a user."""
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        AUTH.destroy_session(user.id)
+        return jsonify({"message": "logged out"}), 200
+    abort(403)
 
 
 if __name__ == "__main__":

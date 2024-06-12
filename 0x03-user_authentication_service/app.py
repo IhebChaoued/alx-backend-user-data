b035ceb449bc3add1c4f55240e56f2d9ee9006d5
Iheb Chaoued <ihebchaoued@gmail.com>
2024-06-12 14:50:11 +0000

    updated

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,66 +2,15 @@
 """
 Basic Flask app
 """
-from flask import Flask, jsonify, request, abort
-from auth import Auth
+from flask import Flask, jsonify
 
 app = Flask(__name__)
-AUTH = Auth()
 
 
 @app.route("/", methods=["GET"])
 def welcome():
     """GET route that returns a welcome message."""
     return jsonify({"message": "Bienvenue"})
-
-
-@app.route("/users", methods=["POST"])
-def register_user():
-    """Endpoint to register a user."""
-    email = request.form.get("email")
-    password = request.form.get("password")
-
-    try:
-        user = AUTH.register_user(email, password)
-        return jsonify({"email": user.email, "message": "user created"}), 200
-    except ValueError as e:
-        return jsonify({"message": str(e)}), 400
-
-
-@app.route("/sessions", methods=["POST"])
-def login():
-    """Endpoint to log in a user."""
-    email = request.form.get("email")
-    password = request.form.get("password")
-
-    if AUTH.valid_login(email, password):
-        session_id = AUTH.create_session(email)
-        if session_id:
-            response = jsonify({"email": email, "message": "logged in"})
-            response.set_cookie("session_id", session_id)
-            return response, 200
-    abort(401)
-
-
-@app.route("/profile", methods=["GET"])
-def profile():
-    """Endpoint to retrieve user profile."""
-    session_id = request.cookies.get("session_id")
-    user = AUTH.get_user_from_session_id(session_id)
-    if user:
-        return jsonify({"email": user.email}), 200
-    abort(403)
-
-
-@app.route("/sessions", methods=["DELETE"])
-def logout():
-    """Endpoint to log out a user."""
-    session_id = request.cookies.get("session_id")
-    user = AUTH.get_user_from_session_id(session_id)
-    if user:
-        AUTH.destroy_session(user.id)
-        return jsonify({"message": "logged out"}), 200
-    abort(403)
 
 
 if __name__ == "__main__":

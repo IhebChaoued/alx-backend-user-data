5a8d2df6a954a8129d06eaac7ed39eb74f57f6ac
Iheb Chaoued <ihebchaoued@gmail.com>
2024-06-12 14:58:20 +0000

    log in

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -18,22 +18,13 @@ def welcome():
 @app.route("/sessions", methods=["POST"])
 def login():
     """POST route to log in a user."""
-    email = request.form.get("email")
-    password = request.form.get("password")
-
-    if not email or not password:
-        abort(400)
-
-    if AUTH.valid_login(email, password):
-        session_id = AUTH.create_session(email)
-        if session_id:
-            response = jsonify({"email": email, "message": "logged in"})
-            response.set_cookie("session_id", session_id)
-            return response
-        else:
-            abort(500)
-    else:
+    email, password = request.form.get("email"), request.form.get("password")
+    if not AUTH.valid_login(email, password):
         abort(401)
+    session_id = AUTH.create_session(email)
+    response = jsonify({"email": email, "message": "logged in"})
+    response.set_cookie("session_id", session_id)
+    return response
 
 
 if __name__ == "__main__":
